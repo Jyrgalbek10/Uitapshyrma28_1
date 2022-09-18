f4476135efdcdd33b189d42842af69fa74b264c9
Jyrgalbek10 <110243095+Jyrgalbek10@users.noreply.github.com>
2022-09-18 23:56:13 +0300

    comi-com

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Arrays;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-    }
-}
+      Integer[] integers = {1,2,3,4,5,6,7,8,9};
+       Arrays.stream(integers).filter(integer -> integer % 2==0).forEach(x-> System.out.println(Math.sqrt(x)));
+            }
+        }
+
+
+
+
+
+
